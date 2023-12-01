373fff1ca5869adfe8f8364171013a023e81b29c
elvanromp <empempire3@gmail.com>
2023-12-01 08:26:57 +0700

    krs

--- generatedPdf.php
+++ generatedPdf.php
@@ -2,3 +2,7 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 $pdf = new \Dompdf\Dompdf();
+$pdf->loadHtml('<h1>Hello</h1>');
+$pdf->setPaper("A4", "Portrait");
+$pdf->render('');
+$pdf->stream('tes.pdf', array("Attachment" => FALSE));
